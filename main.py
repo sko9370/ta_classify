cd221949757684f1cf1edf9c61abebdad3cb7ac6
sko9370 <sko9370@ymail.com>
2021-02-15 22:07:39 -0500

    got positive r2_score

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ from ta.volatility import BollingerBands
 from ta.momentum import RSIIndicator
 
 from sklearn.model_selection import train_test_split
-#from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error, r2_score
-
 from xgboost import XGBRegressor
 
-import autosklearn.regression
+from sklearn.metrics import mean_absolute_error, r2_score
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.style.use('seaborn')
 
 def bollinger_bands(df):
     indicator_bb = BollingerBands(close=df["Close"])
-    df['bb_bbm'] = indicator_bb.bollinger_mavg()
-    df['bb_bbh'] = indicator_bb.bollinger_hband()
-    df['bb_bbl'] = indicator_bb.bollinger_lband()
+    #df['bb_bbm'] = indicator_bb.bollinger_mavg()
+    #df['bb_bbh'] = indicator_bb.bollinger_hband()
+    #df['bb_bbl'] = indicator_bb.bollinger_lband()
+    df['bb_bbh'] = indicator_bb.bollinger_hband_indicator()
+    df['bb_bbl'] = indicator_bb.bollinger_lband_indicator()
 
 def rsi(df):
     indicator_rsi = RSIIndicator(close=df["Close"])
@@ -32,9 +34,9 @@ def show_all():
     pd.set_option('display.max_colwidth', None)
 
 if __name__ == '__main__':
-    msft = yf.Ticker("msft")
-    df = msft.history(period="3mo", interval="1d")
-    
+    msft = yf.Ticker("spy")
+    df = msft.history(period="1y", interval="1d")
+
     change(df)
     bollinger_bands(df)
     rsi(df)
@@ -44,7 +46,8 @@ if __name__ == '__main__':
     show_all()
     #print(df[['Close', 'Change', 'bb_bbl', 'bb_bbm', 'bb_bbh', 'rsi']])
 
-    features = ['Close', 'bb_bbl', 'bb_bbm', 'bb_bbh', 'rsi']
+    #features = ['Close', 'bb_bbl', 'bb_bbm', 'bb_bbh', 'rsi']
+    features = ['Close', 'bb_bbl', 'bb_bbh', 'rsi']
     X = df[features].copy()
     y = df['Change'].copy()
 
@@ -66,16 +69,22 @@ if __name__ == '__main__':
     print('MAE: ', mean_absolute_error(val_y, rtr_predictions))
     print('Score: ', r2_score(val_y, rtr_predictions))
 
+    df['rtr'] = rtr_model.predict(df[features])
+
     xgb_model = XGBRegressor()
-    xgb_model.fit(X_train, y_train)
+    xgb_model.fit(train_X, train_y)
     xgb_predictions = xgb_model.predict(val_X)
 
     print('MAE: ', mean_absolute_error(val_y, xgb_predictions))
     print('Score: ', r2_score(val_y, xgb_predictions))
 
-    automl = autosklearn.regression.AutoSklearnRegressor()
-    automl.fit(X_train, y_train)
-    auto_predicitions = automl.predict(val_X)
+    df['xgb'] = xgb_model.predict(df[features])
 
-    print('MAE: ', mean_absolute_error(val_y, auto_predictions))
-    print('Score: ', r2_score(val_y, auto_predictions))
+    plt.plot(df.Change, label='Change')
+    #plt.plot(df.bb_bbl, label='bb_bbl')
+    #plt.plot(df.bb_bbh, label='bb_bbh')
+    plt.plot(df.rtr, label='rtr')
+    plt.plot(df.xgb, label='xgb')
+    plt.title('Results')
+    plt.legend()
+    plt.show()
